61ce42394231dd289148381020ab3462591076e8
aedbel <belmekhfi.aed@gmail.com>
2023-04-17 23:41:13 +0100

    Flutter Adnin Panel commit

---
+++ lib/models/chart_area_data.dart
@@ -0,0 +1,6 @@
+class ChartData {
+  String month ;
+  double amount = 0;
+  
+  ChartData(this.month, this.amount);
+}
---
+++ lib/screens/main/main_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+
+class MainScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
+    return Scaffold();
+  }
+}
